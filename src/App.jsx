ceb751f1b8e831e330af1dba119e05cf4ba3805d
lambeboluwatife <boluwatifelambe@gmail.com>
2023-08-20 17:30:44 +0100

    showcase and jumbotron

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Showcase from "./components/Showcase";
+import Jumbotron from "./components/Jumbotron";
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Showcase />
+              <Jumbotron />
+            </>
+          }
+        />
+      </Routes>
       <Footer />
     </Router>
   );
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron">
+      <h3>Trusted by Small Businesses and Startups</h3>
+      <div className="jumbotron-image">
+        <img className="atica" src="/Atica.png" alt="Atica" />
+        <img className="amara" src="/Amara.png" alt="Amara" />
+        <img className="solaytic" src="/Solaytic.png" alt="Solaytic" />
+        <img className="aven" src="/Aven.png" alt="Aven" />
+        <img className="foxhub" src="/Fox Hub.png" alt="Foxhub" />
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
